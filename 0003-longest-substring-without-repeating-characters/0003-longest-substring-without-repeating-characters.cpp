049d19dfc2dc55c691c82930e77929fcadb129ed
blanket77 <70279365+blanket77@users.noreply.github.com>
2025-03-20 18:30:32 +0900

    Time: 88 ms (9.73%), Space: 22.8 MB (8.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,20 @@
 class Solution {
-    public:
-        int lengthOfLongestSubstring(string s) {
-            int maxs = 0;
-            for(int i = 0; i < s.size(); i++) {
-                string substr = "";
-                for(int j = i; j < s.size(); j++) {
-                    if(substr.find(s[j]) == string::npos) {
-                        substr += s[j];
-                    } else {
-                        maxs = max(maxs, (int)substr.size());
-                        break;
-                    }
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxs = 0;
+        for (int i = 0; i < s.size(); i++) {
+            string substr = "";
+            for (int j = i; j < s.size(); j++) {
+                if (substr.find(s[j]) == string::npos) {
+                    substr += s[j];
+                } else {
+                    maxs = max(maxs, (int)substr.size());
+                    break;
                 }
             }
-            return maxs;
+            // 루프가 끝났는데 break 없이 끝났을 수도 있으니 여기서도 max 확인
+            maxs = max(maxs, (int)substr.size());
         }
-    };
+        return maxs;
+    }
+};
